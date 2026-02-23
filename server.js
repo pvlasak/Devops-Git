c2fbd84d167ed203182c24ec20da3ffd59fcc437
pvlasak <petr.vlasakk@gmail.com>
2026-02-23 22:29:03 +0100

    const app and server creation

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 console.log("Hello I am server.js file")
 console.log("End of the file")
 console.lgo("GoodBye")
+
+const express=require('express')
+const app = express()
+const server =app.listen(3000, function() {
+	console.log("app is listening on port 3000."
+	}
+
